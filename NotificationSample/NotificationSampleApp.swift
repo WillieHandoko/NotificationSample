7a390ef731de2b0f0a99b6785119875b5855f94f
WILLIAM JONATHAN HANDOKO <108049958+WillieHandoko@users.noreply.github.com>
2024-10-29 03:48:50 +0700

    Initial Commit

---
+++ NotificationSample/NotificationSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationSampleApp.swift
+//  NotificationSample
+//
+//  Created by William Handoko on 29/10/24.
+//
+
+import SwiftUI
+
+@main
+struct NotificationSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
